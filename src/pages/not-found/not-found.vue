79a25c16f69acf16227483fbc4ecec5f4c906b87
谢佳倩 <likej2ee@qq.com>
2018-03-13 16:56:41 +0800

    404页面

--- src/pages/not-found/not-found.vue
+++ src/pages/not-found/not-found.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    Not Found Page 404
+  <div class="error">
+    <img class="e-404" v-lazy="bgimg" alt="">
+    <ul class="text">
+      <li>There was a problem</li>
+      <li>The page you are looking for is not here or moved.</li>
+    </ul>
+    <div class="back-home">
+      <i class="icon-home"></i>
+      <a href="/">Back to homepage</a>
+    </div>
   </div>
 </template>
 
@@ -10,9 +18,26 @@ import { utils, constants, app } from "../../core/ts/app";
 import { errorHandler } from "../../core/ts/services";
 
 @Component
-export default class MyComponent extends Vue {}
+export default class MyComponent extends Vue {
+  bgimg: string = require("./images/404.png");
+}
 </script>
 
 <style lang="scss" scoped>
-
+.error {
+  text-align: center;
+}
+.text {
+  color: #999;
+}
+.icon-home {
+  display: inline-block;
+  background-image: url(images/home.png);
+  width: 48px;
+  height: 30px;
+}
+.back-home {
+  font-size: 18px;
+  padding-top: 10px;
+}
 </style>
